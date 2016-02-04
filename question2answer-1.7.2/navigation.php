439f5e6b83ff58daf059b1e7a1cd8c524e03105a
Arik Coats <adcoats@iastate.edu>
2016-02-04 12:54:33 -0600

    Home page now orders links by name

--- question2answer-1.7.2/navigation.php
+++ question2answer-1.7.2/navigation.php
@@ -6,16 +6,20 @@
 	$type = $_POST["type"];
 	$id = $_POST["id"];
 	if ($type == "home") {
-		$result = qa_db_query_raw("SELECT * FROM `library` ORDER BY ID");
+		//$result = qa_db_query_raw("SELECT * FROM `library` ORDER BY ID");
+		$result = qa_db_query_raw("SELECT * FROM `library` ORDER BY Name");
 	}
 	else if ($type == "library") {
-		$result = qa_db_query_raw("SELECT ID, Name FROM `package` WHERE LID = " . $id . " ORDER BY ID");
+		//$result = qa_db_query_raw("SELECT ID, Name FROM `package` WHERE LID = " . $id . " ORDER BY ID");
+		$result = qa_db_query_raw("SELECT ID, Name FROM `package` WHERE LID = " . $id . " ORDER BY Name");
 	}
 	else if ($type == "package") {
-		$result = qa_db_query_raw("SELECT ID, Name FROM `type` WHERE PID = " . $id . " ORDER BY ID");
+		//$result = qa_db_query_raw("SELECT ID, Name FROM `type` WHERE PID = " . $id . " ORDER BY ID");
+		$result = qa_db_query_raw("SELECT ID, Name FROM `type` WHERE PID = " . $id . " ORDER BY Name");
 	}
 	else if ($type == "class") {
-		$result = qa_db_query_raw("SELECT ID, Name, Arguments FROM `method` WHERE TID = " . $id . " ORDER BY ID");
+		//$result = qa_db_query_raw("SELECT ID, Name, Arguments FROM `method` WHERE TID = " . $id . " ORDER BY ID");
+		$result = qa_db_query_raw("SELECT ID, Name, Arguments FROM `method` WHERE TID = " . $id . " ORDER BY Name");
 	}
 
 	
